eca6bb874b59d2b87b2297ff138dd33c8221f052
maingocvinh <97122860+maingocvinh@users.noreply.github.com>
2022-12-28 23:47:39 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,8 @@ export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 mkdir output
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output menuconfig
 make -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
-
+./tools/dtbTool -s 2048 -o output/arch/arm/boot/dtb -p output/scripts/dtc/ output/arch/arm/boot/dts/
