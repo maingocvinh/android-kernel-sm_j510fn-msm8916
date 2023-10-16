d370ff645620106c0158143bf6b8025762dbb9bd
Mai Ngọc Vinh <netbat4399@gmail.com>
2023-10-16 21:18:37 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,8 @@ export ARCH=arm
 export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+# make -C $(pwd) clean && make mrproper
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima_defconfig
 make -C $(pwd) O=output menuconfig
 make -C $(pwd) O=output
 
